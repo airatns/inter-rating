98ae8601b4be3cbd776c4426ff2d5b1cf4adb903
Раиса Буданаева <satarovara@yandex.ru>
2022-05-20 21:56:12 +0300

    fixed reviewer's points about filter

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,26 +3,14 @@ from reviews.models import Title
 
 
 class TitleFilter(filters.FilterSet):
-    genre = filters.CharFilter(field_name="genre__slug", method='filter_genre')
-    category = filters.CharFilter(
-        field_name="category__slug",
-        method='filter_category'
-    )
-    name = filters.CharFilter(field_name="name", method='filter_name')
-    year = filters.CharFilter(field_name="year", method='filter_year')
+    genre = filters.CharFilter(field_name="genre__slug",
+                               lookup_expr='icontains')
+    category = filters.CharFilter(field_name="category__slug",
+                                  lookup_expr='icontains'
+                                  )
+    name = filters.CharFilter(field_name="name", lookup_expr='icontains')
+    year = filters.CharFilter(field_name="year", lookup_expr='icontains')
 
     class Meta:
         model = Title
         fields = ['genre', 'category', 'year', 'name']
-
-    def filter_genre(self, queryset, name, genre):
-        return queryset.filter(genre__slug__contains=genre)
-
-    def filter_category(self, queryset, name, category):
-        return queryset.filter(category__slug__contains=category)
-
-    def filter_name(self, queryset, name, value):
-        return queryset.filter(name__contains=value)
-
-    def filter_year(self, queryset, name, value):
-        return queryset.filter(year__contains=value)
